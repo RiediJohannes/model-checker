d141c37a1914938021ff45e74900b16ad88a92aa
RiediJohannes <johannes.riedmann.vlbg@gmail.com>
2026-02-14 11:06:10 +0100

    Added unit tests to check logic types

--- src/logic/types.rs
+++ src/logic/types.rs
@@ -23,6 +23,16 @@ impl Clause {
         Self { lits: v.into_boxed_slice() }
     }
 }
+impl<const N: usize> From<[Literal; N]> for Clause {
+    fn from(arr: [Literal; N]) -> Self {
+        Self::new(arr)
+    }
+}
+impl From<Literal> for Clause {
+    fn from(lit: Literal) -> Self {
+        Self::new([lit])
+    }
+}
 impl Debug for Clause {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "Clause: [{}]",
@@ -98,6 +108,16 @@ impl From<Clause> for CNF {
         CNF::from(vec![clause])
     }
 }
+impl<C> FromIterator<C> for CNF
+where
+    C: Into<Clause>
+{
+    fn from_iter<T: IntoIterator<Item = C>>(iter: T) -> Self {
+        let clauses = iter.into_iter().map(|c| c.into()).collect();
+        Self { clauses }
+    }
+}
+
 impl Debug for CNF {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "CNF: {{ {} }}",
@@ -142,6 +162,15 @@ impl BitAnd<&CNF> for &CNF {
     }
 }
 
+#[macro_export]
+macro_rules! cnf {
+    ( $( [ $( $lit:expr ),* ] ),* ) => {
+        $crate::logic::CNF::from(vec![
+            $( $crate::logic::Clause::from([ $( $lit ),* ]) ),*
+        ])
+    };
+}
+
 
 /// Short for Extended CNF.</br>
 /// Extends a formula in CNF by an output tseitin literal that is true iff the formula is satisfiable.
@@ -174,5 +203,116 @@ impl From<Literal> for XCNF {
 impl Debug for XCNF {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "XCNF {{ clauses: {{ {:?} }}, out_lit: {} }}", self.clauses, self.out_lit)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cnf_from_clause() {
+        let a = Literal::from_var(1);
+        let b = Literal::from_var(2);
+        let c = Literal::from_var(3);
+        let clause: Clause = Clause::new([a, -b, c]);
+
+        let cnf = CNF::from(clause.clone());
+        assert_eq!(cnf.clauses.len(), 1);
+        assert_eq!(cnf.clauses[0].lits.len(), 3);
+        assert_eq!(cnf.clauses[0], clause);
+    }
+
+    #[test]
+    fn cnf_from_unit() {
+        let a = Literal::from_var(1);
+        let cnf = CNF::from(a);
+        assert_eq!(cnf.clauses.len(), 1);
+        assert_eq!(cnf.clauses[0].lits.len(), 1);
+        assert_eq!(cnf.clauses[0].lits[0], a);
+
+        let b = Literal::from_var(2);
+        let cnf = CNF::from(-b);
+        assert_eq!(cnf.clauses.len(), 1);
+        assert_eq!(cnf.clauses[0].lits.len(), 1);
+        assert_eq!(cnf.clauses[0].lits[0], -b);
+    }
+
+    #[test]
+    fn xcnf_from_unit() {
+        let a = Literal::from_var(1);
+        let xcnf = XCNF::from(a);
+        assert_eq!(xcnf.clauses.len(), 1);
+        assert_eq!(xcnf.clauses[0].lits.len(), 1);
+        assert_eq!(xcnf.clauses[0].lits[0], a);
+        assert_eq!(xcnf.out_lit, a);
+
+        let b = Literal::from_var(2);
+        let xcnf = XCNF::from(-b);
+        assert_eq!(xcnf.clauses.len(), 1);
+        assert_eq!(xcnf.clauses[0].lits.len(), 1);
+        assert_eq!(xcnf.clauses[0].lits[0], -b);
+        assert_eq!(xcnf.out_lit, -b);
+    }
+
+    #[test]
+    fn cnf_and_cnf() {
+        // Case 1: Unit clauses
+        let a = Literal::from_var(1);
+        let b = Literal::from_var(2);
+        let c = Literal::from_var(3);
+
+        let left_cnf = cnf![[a, b]];
+        let right_cnf = CNF::from(c);
+
+        let result: CNF = &left_cnf & &right_cnf;
+        assert_eq!(result.clauses.len(), left_cnf.clauses.len() + right_cnf.clauses.len());
+        assert_eq!(result.clauses.len(), 2);
+        assert_eq!(&result.clauses[0], &left_cnf.clauses[0]);
+        assert_eq!(&result.clauses[1], &right_cnf.clauses[0]);
+
+
+        // Case 2: More complex clauses
+        let left_cnf = cnf![[-a, b]];
+        let right_cnf = cnf![[-b, c], [-c, a]];
+
+        let result: CNF = &left_cnf & &right_cnf;
+        assert_eq!(result.clauses.len(), left_cnf.clauses.len() + right_cnf.clauses.len());
+        assert_eq!(result.clauses.len(), 3);
+        assert_eq!(&result.clauses[0], &left_cnf.clauses[0]);
+        assert_eq!(&result.clauses[1], &right_cnf.clauses[0]);
+        assert_eq!(&result.clauses[2], &right_cnf.clauses[1]);
+    }
+
+    #[test]
+    fn cnf_and_clause() {
+        let a = Literal::from_var(1);
+        let b = Literal::from_var(2);
+        let c = Literal::from_var(3);
+
+        let cnf = cnf![[a, b], [-b]];
+        let clause = Clause::from([a, c]);
+
+        let result: CNF = &cnf & &clause;
+        assert_eq!(result.clauses.len(), 3);
+        assert_eq!(&result.clauses[0], &cnf.clauses[0]);
+        assert_eq!(&result.clauses[1], &cnf.clauses[1]);
+        assert_eq!(&result.clauses[2], &clause);
+    }
+
+    #[test]
+    fn clause_and_clause() {
+        let a = Literal::from_var(1);
+        let b = Literal::from_var(2);
+        let c = Literal::from_var(3);
+
+        let left_clause = Clause::from([-a, b]);
+        let right_clause = Clause::from([-b, c]);
+
+        let result: CNF = &left_clause & &right_clause;
+        assert_eq!(result.clauses.len(), 2);
+        assert_eq!(&result.clauses[0], &left_clause);
+        assert_eq!(&result.clauses[1], &right_clause);
     }
 }
